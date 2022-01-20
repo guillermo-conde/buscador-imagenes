6ac6595de5a83a46c622a38783b6443ff5778d47
Guillermo Conde Flores <condeguillermo8@gmail.com>
2022-01-20 16:02:31 -0600

    Se agrega módulo Formik y estilos al input de búsqueda

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Field, Form, Formik } from "formik";
+import './Header.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Formik
+          initialValues={{search: ''}}
+          onSubmit={async values => {
+            console.log(values);
+          }}>
+          <Form>
+            <Field name='search' />
+          </Form>
+        </Formik>
       </header>
     </div>
   );
