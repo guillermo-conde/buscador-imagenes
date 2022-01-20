69f0dd9bf59fa7d12d6242f63ded1aa9d119b07a
Guillermo Conde Flores <condeguillermo8@gmail.com>
2022-01-20 16:14:48 -0600

    Se hace la petición asíncrona a la API de unsplash y se asigna a un estado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useState} from "react";
 import { Field, Form, Formik } from "formik";
 import './Header.css';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  console.log({photos});
   return (
     <div>
       <header>
         <Formik
           initialValues={{search: ''}}
           onSubmit={async values => {
-            console.log(values);
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+              headers: {
+                'Authorization': 'Client-ID Ld0C8pYwYpBKJEiutHBAQdpj9XLRgcoDaC_o7LLutVM',
+              }
+            });
+            const data = await response.json();
+            setPhotos(data.results);
           }}>
           <Form>
             <Field name='search' />
